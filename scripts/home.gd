d795e1476901e188b196e5e2893a215173ad2e7e
rizwaradnan03 <rizwar.adnanbyantara@gmail.com>
2025-05-15 01:12:23 +0700

    Finished Change Scene

---
+++ scripts/home.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Player.PLAYER_PACKAGED:
+		var player = Player.PLAYER_PACKAGED
+		add_child(player)
+		Player.PLAYER_PACKAGED = null
+		$Player.position = $SpawnPoint.position
+		print("bji")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/house.gd
+++ scripts/house.gd
@@ -1,14 +1,7 @@
 extends StaticBody2D
 
-func _process(delta: float) -> void:
-	if Player.PLAYER_PACKAGED:
-		var player = Player.PLAYER_PACKAGED
-		add_child(player)
-		Player.PLAYER_PACKAGED = null
-		player.position = Vector2(3, -5)
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		Player.PLAYER_PACKAGED = body
 		body.get_parent().remove_child(body)
-		get_tree().change_scene_to_file("res://objects/properties/house.tscn")
+		get_tree().change_scene_to_file("res://scenes/home.tscn")
